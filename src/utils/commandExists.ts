571c4a407dffc24d0c18b82ad7cdfd5698015536
miguelgargallo <andorra.mg@gmail.com>
2023-01-26 02:55:36 +0100

    gh 1.0.0 | npm 0.0.0 | Source Code

--- src/utils/commandExists.ts
+++ src/utils/commandExists.ts
@@ -1,7 +1,7 @@
-import * as bin from './bin';
+import * as bins from './bin';
 
 export const commandExists = (command: string) => {
-  const commands = ['clear', ...Object.keys(bin)];
+  const commands = ['clear', ...Object.keys(bins)];
 
   return commands.indexOf(command.split(' ')[0]) !== -1;
 };
